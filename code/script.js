8630e0fc304874dbab4574d08af789e12965dbdb
Tom Baty <tbaty@myges.fr>
2023-11-04 14:54:11 +0100

    Suppresion dosier node

--- code/script.js
+++ code/script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (messageElement) {
         // Affiche un message dans la console
         console.log("Le script JavaScript fonctionne !");
-        
+        console.log("Le script JavaScript fonctionne !");
         // Modifie le contenu de l'élément HTML avec un message
         messageElement.textContent = "Ce script fonctionne à merveille !";
     }
